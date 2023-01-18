bfae9a8eced05af4209db749e0513a7e3dfc5c0b
allipope <allipope21@gmail.com>
2023-01-17 17:12:40 -0700

    Add project files.

---
+++ Mission2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Dice Throwing Simulator!");
+            Console.WriteLine("How many dice rolls would you like to simulate? ");
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS ");
+            Console.WriteLine("Each ' * ' represents 1 % of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + numRolls);
+            Console.WriteLine("2: " + );
+            Console.WriteLine("3: ");
+            Console.WriteLine("4: ");
+            Console.WriteLine("5: ");
+            Console.WriteLine("6: ");
+            Console.WriteLine("7: ");
+            Console.WriteLine("8: ");
+            Console.WriteLine("9: ");
+            Console.WriteLine("10: ");
+            Console.WriteLine("11: ");
+            Console.WriteLine("12: ");
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
